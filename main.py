b7168116ef17edb1112637c449a8599e0a792a1e
Nikita.Zmanovskiy <72348175+test212366@users.noreply.github.com>
2023-10-24 21:59:37 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import django
+
+
+
+print(django.get_version())
+#to create virtual env with django i must to use - mkvirtualenv myapp
+#django-admin startproject project
+ 
+#workon myapp
+
+#python manage.py runserver
+
+
+
+
+#django-admin startproject blog
+#python manage.py startapp posts
+#python manage.py runserver
